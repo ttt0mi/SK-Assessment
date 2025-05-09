90b13e838d418f272d74f97634b9b742933b44a5
tomi <tonmide96@gmail.com>
2025-05-09 08:21:39 +0100

    accidentally deleted

--- Chapter 3 Assessments/mean_median_mode.py
+++ Chapter 3 Assessments/mean_median_mode.py
@@ -1,17 +1,15 @@
 values = [9, 11, 22, 34, 17, 22, 34, 22, 40]
 
-
 mean = sum(values) / 3
 
 sorted_values = sort(values)
-sorted_values_length = len(sorted_values)
 
-median = 
+median =	()
 
 
 print(mean)
 print(median)
-print(mode)
+#print(mode)
 
 
 
---
+++ Chapter 3 Assessments/test.py
@@ -0,0 +1,44 @@
+
+for row in range(10):
+	for column in range(row):
+		print("*", end = "")
+	
+		for row in range(10):
+			for column in range(10):
+				print(" ", end = "")
+			print()
+
+
+		for row in range(10):
+			for column in range(row, 10):
+				print("*", end = "")
+			print()
+
+
+		for row in range(10):
+			for column in range(10):
+				print(" ", end = "")
+			print()
+
+
+		for row in range(10):
+			for column in range(row):
+				print(" ", end = "")
+			for column in range(row, 10):
+				print("*", end = "")
+			print()
+
+
+		for row in range(10):
+			for column in range(10):
+				print(" ", end = "")
+			print()
+
+
+		for row in range(10):
+			for column in range(row, 10):
+				print(" ", end = "")
+			for column in range(row):
+				print("*", end = "")
+			print()
+		
